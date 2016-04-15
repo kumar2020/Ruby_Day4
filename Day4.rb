f170b6978d0c52e342d1f63201fb798f9aae1ef5
jagesh <kumar.jagesh@gmail.com>
2016-04-15 11:21:09 -0700

    this is first case

--- Day4.rb
+++ Day4.rb
@@ -1,5 +1,27 @@
+class Person
 
-
-a=5
-b=7
-a+b
+	def male
+		puts "Intelligent than other animals"
+	end
+end
+jagesh=Person.new
+jagesh.male
+###################
+class Addition
+	def add
+		a=23
+		b=45
+		
+		print c=a+b
+	end
+end
+d=Addition.new
+d.add
+###################	
+class Multiple
+	def multi(a,b) 
+		puts m=a*b
+	end
+end
+c=Multiple.new
+c.multi(23,21)
